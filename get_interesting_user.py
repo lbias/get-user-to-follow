4b1aaaa065984fa23dc7d62585a0af37c0220f0f
lbias <306508354@qq.com>
2017-10-23 18:05:50 +0800

    get user id of a medium username

--- get_interesting_user.py
+++ get_interesting_user.py
@@ -1,7 +1,20 @@
 import json
+import requests
+
+MEDIUM = 'https://medium.com'
 
 
 # Removes the extra characters that get returned with every JSON request on Medium endpoints
 # json to dictionary
 def clean_json_response(response):
     return json.loads(response.text.replace('])}while(1);</x>', '', 1))
+
+
+# Returns the User ID of a Medium Username
+def get_user_id(username):
+    print('Retrieving user ID...')
+
+    url = MEDIUM + '/@' + username + '?format=json'
+    response = requests.get(url)
+    response_dict = clean_json_response(response)
+    return response_dict['payload']['user']['userId']
