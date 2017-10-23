1b06b47de83be64b0a10204461cbbd275f84060d
lbias <306508354@qq.com>
2017-10-23 18:09:32 +0800

    get a list of users from a user's followings

--- get_interesting_user.py
+++ get_interesting_user.py
@@ -18,3 +18,32 @@ def get_user_id(username):
     response = requests.get(url)
     response_dict = clean_json_response(response)
     return response_dict['payload']['user']['userId']
+
+
+# Returns the list of Usernames from a user's Followings list
+def get_list_of_followings(user_id):
+    print('Retrieving users from Followings...')
+    
+    next_id = False
+    followings = []
+
+    while True:
+        if next_id:
+            # If this is not the first page of the followings list
+            url = MEDIUM + '/_/api/users/' + user_id + '/following?limit=8&to=' + next_id
+        else:
+            # If this is the first page of the followings list
+            url = MEDIUM + '/_/api/users/' + user_id + '/following'
+
+        response = requests.get(url)
+        response_dict = clean_json_response(response)
+
+        for user in response_dict['payload']['value']:
+            followings.append(user['username'])
+
+        try:
+            # If the "to" key is missing, we've reached the end of the list and an exception is thrown
+            next_id = response_dict['payload']['paging']['next']['to']
+        except:
+            break
+    return followings
